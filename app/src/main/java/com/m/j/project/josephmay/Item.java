e2ff1ec4615fdd346cbeea29e37383856a2d3f2a
jmay24 <Jokiemay1>
2016-12-07 13:57:53 +0000

    updated styles

---
+++ app/src/main/java/com/m/j/project/josephmay/Item.java
@@ -0,0 +1,24 @@
+package com.m.j.project.josephmay;
+
+/**
+ * Created by myadmin on 07/12/2016.
+ */
+
+public class Item {
+
+    private String title;
+
+    public Item() {}
+
+    public Item(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
